d8f37cd137db4deca691b42ac7433901157bcf8c
Kavita Laddad <kladdad@users.noreply.github.com>
2022-10-29 15:40:10 -0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+    },
+    minWidth: {
+      '0': '0',
+      '1/4': '25%',
+      '1/3': '30%',
+      '1/2': '50%',
+      '70': '70%',
+      '3/4': '75%',
+      'full': '100%',
+    },
+    screens: {
+      'xs': '550px',
+      ...defaultTheme.screens,
+    }
+  },
+  variants: {
+    extend: {
+      opacity: ['disabled'],
+    },
+  },
+  plugins: [],
+}
