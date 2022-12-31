4a244bf0d7afe3102d438c51a1c77e7833ed1b1c
Kavita Laddad <kavita@asavari.local>
2022-12-30 18:38:07 -0800

    Fix a Next.js warning by setting the priority attribute for the logo image

--- components/navbar/logo.tsx
+++ components/navbar/logo.tsx
@@ -2,5 +2,5 @@ import React, { FC } from "react";
 import Image from "next/image";
 
 export const Logo: FC = () => {
-  return <Image src="/images/logo.png" width={200} height={46} alt="Baithak" />;
+  return <Image src="/images/logo.png" width={200} height={46} alt="Baithak" priority />;
 };
