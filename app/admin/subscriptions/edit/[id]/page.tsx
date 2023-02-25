a84a0babbd01baf473003bc68898f1e9d75edf78
Kavita Laddad <kladdad@users.noreply.github.com>
2023-02-25 10:05:03 -0800

    Implement subscription editing

---
+++ app/admin/subscriptions/edit/[id]/page.tsx
@@ -0,0 +1,111 @@
+"use client";
+
+import React from "react";
+import { useRouter } from "next/navigation";
+import { useQuery, useMutation } from "@apollo/react-hooks";
+import Protected from "../../../../../components/auth/protected";
+import { getFragmentData, graphql } from "../../../../../__generated__";
+import {
+  SearchSubscriptionDocument,
+  SubscriptionDetailsFragment,
+} from "../../../../../__generated__/graphql";
+import SubscriptionEditor from "../../../../../components/subscriptions/subscriptionEditor";
+
+interface EditSubscriptionProps {
+  params: { id: string };
+}
+
+export default function EditSubscription(props: EditSubscriptionProps) {
+  const id = props.params.id === "new" ? props.params.id : parseInt(props.params.id);
+
+  return (
+    <Protected>{id === "new" ? <CreateSubscription /> : <UpdateSubscription id={id} />}</Protected>
+  );
+}
+
+interface UpdateSubscriptionProps {
+  id: number;
+}
+
+function UpdateSubscription({ id }: UpdateSubscriptionProps) {
+  const router = useRouter();
+
+  let { data, loading } = useQuery(getSubscription, {
+    variables: { id },
+  });
+
+  const [updateSubscriptionMutation] = useMutation(updateSubscription);
+
+  const saveSubscription = async (subscription: SubscriptionDetailsFragment) => {
+    updateSubscriptionMutation({
+      variables: {
+        id,
+        data: subscription,
+      },
+    }).then(() => router.back());
+  };
+
+  if (!data || !data.subscription || loading) {
+    return <>"Loading..."</>;
+  }
+  const subscription = getFragmentData(subscriptionDetails, data?.subscription);
+
+  return (
+    <>
+      subscription && (
+      <SubscriptionEditor id={id} subscription={subscription} done={saveSubscription} />)
+    </>
+  );
+}
+
+function CreateSubscription() {
+  const router = useRouter();
+
+  const [addSubscription] = useMutation(createSubscription, {
+    refetchQueries: [{ query: SearchSubscriptionDocument, variables: { search: "%%" } }],
+  });
+
+  const saveSubscription = (subscription: SubscriptionDetailsFragment) => {
+    addSubscription({
+      variables: {
+        data: subscription,
+      },
+    }).then((data) => router.back());
+  };
+
+  return <SubscriptionEditor done={saveSubscription} />;
+}
+
+const subscriptionDetails = graphql(`
+  fragment SubscriptionDetails on Subscription {
+    email
+    group
+  }
+`);
+
+const getSubscription = graphql(`
+  query getSubscription($id: Int!) {
+    subscription(id: $id) {
+      id
+      ...SubscriptionDetails
+    }
+  }
+`);
+
+const updateSubscription = graphql(`
+  mutation updateSubscription($id: Int!, $data: SubscriptionUpdateInput!) {
+    updateSubscription(id: $id, data: $data) {
+      id
+      ...SubscriptionDetails
+    }
+  }
+`);
+
+const createSubscription = graphql(`
+  mutation createSubscription($data: SubscriptionCreationInput!) {
+    createSubscription(data: $data) {
+      id
+      ...SubscriptionDetails
+    }
+  }
+`);
--- app/admin/subscriptions/page.tsx
+++ app/admin/subscriptions/page.tsx
@@ -1,7 +1,48 @@
-import React, { FC } from "react";
+"use client";
 
-const Subscriptions: FC = () => {
-  return <div className="main-container">Subscriptions</div>;
-};
+import React from "react";
+import EntityList, { EntityInfo } from "../../../components/common/entityList";
+import Protected from "../../../components/auth/protected";
+import { graphql } from "../../../__generated__";
+import { SubscriptionDetailsFragment } from "../../../__generated__/graphql";
 
-export default Subscriptions;
+export default function Subscriptions() {
+  const descFn = (subscription: SubscriptionDetailsFragment) => {
+    const group = subscription.group ? subscription.group : "All";
+    return `${subscription.email} (${group})`;
+  };
+
+  return (
+    <Protected>
+      <EntityList
+        entityInfo={
+          new EntityInfo(
+            "Subscription",
+            "Subscriptions",
+            "subscriptions",
+            searchSubscription,
+            deleteSubscription
+          )
+        }
+        descFn={descFn}
+      />
+    </Protected>
+  );
+}
+
+const deleteSubscription = graphql(`
+  mutation deleteSubscription($id: Int!) {
+    deleteSubscription(id: $id) {
+      id
+    }
+  }
+`);
+
+const searchSubscription = graphql(`
+  query searchSubscription($search: String) {
+    subscriptions(where: { email: { like: $search } }, orderBy: { email: ASC }) {
+      id
+      ...SubscriptionDetails
+    }
+  }
+`);
