ada287ba2cb85547e18c238342bf24d372bed362
Kavita Laddad <kladdad@users.noreply.github.com>
2023-02-25 13:59:59 -0800

    Implement notification editor

--- app/admin/notifications/page.tsx
+++ app/admin/notifications/page.tsx
@@ -1,7 +1,48 @@
-import React, { FC } from "react";
+"use client";
 
-const Notifications: FC = () => {
-  return <div className="main-container">Notifications</div>;
-};
+import React from "react";
+import { graphql } from "../../../__generated__";
+import Protected from "../../../components/auth/protected";
+import EntityList, { EntityInfo } from "../../../components/common/entityList";
+import { getNotifications } from "../../../graphql/notifications";
+import { NotificationShortDetailsFragment } from "../../../__generated__/graphql";
 
-export default Notifications;
+export default function NotificationList() {
+  function cleanupNotifications(notifications: NotificationShortDetailsFragment[]) {
+    // Currently Clay may return same entity multiple times for this query so create an array of unique elements
+    return [...new Set(notifications)];
+  }
+
+  const descFn = (notification: NotificationShortDetailsFragment) => {
+    return notification.subject;
+  };
+
+  return (
+    <Protected>
+      <EntityList
+        entityInfo={
+          new EntityInfo(
+            "Notification",
+            "Notifications",
+            "notifications",
+            getNotifications,
+            deleteNotification,
+            undefined,
+            undefined,
+            cleanupNotifications
+            // deleteNotificationCacheUpdate
+          )
+        }
+        descFn={descFn}
+      />
+    </Protected>
+  );
+}
+
+const deleteNotification = graphql(`
+  mutation deleteNotification($id: Int!) {
+    deleteNotification(id: $id) {
+      id
+    }
+  }
+`);
