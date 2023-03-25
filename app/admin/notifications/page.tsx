e4e942df018b2487fefe997b4bdbacd404b36ef9
Kavita Laddad <kavita@asavari.local>
2023-03-25 09:28:17 -0700

    Remove unneeded cleanup from notification list

--- app/admin/notifications/page.tsx
+++ app/admin/notifications/page.tsx
@@ -7,11 +7,6 @@ import { getNotifications } from "../../../graphql/notifications";
 import { NotificationShortDetailsFragment } from "../../../__generated__/graphql";
 
 export default function NotificationList() {
-  function cleanupNotifications(notifications: NotificationShortDetailsFragment[]) {
-    // Currently Clay may return same entity multiple times for this query so create an array of unique elements
-    return [...new Set(notifications)];
-  }
-
   const descFn = (notification: NotificationShortDetailsFragment) => {
     return notification.subject;
   };
@@ -26,7 +21,7 @@ export default function NotificationList() {
           getNotifications,
           deleteNotification,
           undefined,
-          cleanupNotifications
+          true
         )
       }
       descFn={descFn}
