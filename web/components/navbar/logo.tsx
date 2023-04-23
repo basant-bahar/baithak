a60ccf9430f3e58345727a43fd1278dc941d8c0e
Kavita Laddad <kavita@asavari.local>
2023-04-22 17:48:31 -0700

    Use function style to define logo component

--- web/components/navbar/logo.tsx
+++ web/components/navbar/logo.tsx
@@ -1,6 +1,6 @@
-import React, { FC } from "react";
+import React from "react";
 import Image from "next/image";
 
-export const Logo: FC = () => {
+export default function Logo() {
   return <Image src="/images/logo.png" width={200} height={46} alt="Baithak" priority />;
-};
+}
