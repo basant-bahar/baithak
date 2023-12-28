b0815da55f0ba5c77d8786058e4651870f39af42
Kavita Laddad <kladdad@users.noreply.github.com>
2023-12-27 16:00:42 -0800

    Turn off caching in Apollo client's ssr mode

--- web/app/apollo-client.js
+++ web/app/apollo-client.js
@@ -1,5 +1,19 @@
 import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+
+const defaultOptions = {
+  watchQuery: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'all',
+  },
+  query: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'all',
+  },
+  mutate: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'all',
+  },
+};
 
 export const ssrApolloClient = new ApolloClient({
   ssrMode: true,
@@ -10,28 +24,5 @@ export const ssrApolloClient = new ApolloClient({
     },
   }),
   cache: new InMemoryCache(),
-});
-
-const httpLink = createHttpLink({
-  uri: process.env.NEXT_PUBLIC_API_URL,
-});
-
-const authLink = setContext((_, { headers }) => {
-  if (localStorage) {
-    // get the authentication token from local storage if it exists
-    const token = localStorage.getItem("token");
-    // return the headers to the context so httpLink can read them
-    return {
-      headers: {
-        ...headers,
-        authorization: token ? `Bearer ${token}` : "",
-      }
-    }
-  }
-});
-
-export const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-  connectToDevTools: process.env.NEXT_PUBLIC_APOLLO_CONNECT_TO_DEV_TOOLS,
+  defaultOptions
 });
